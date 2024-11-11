99249acf4ea2a0bcb8f93994c7c1886012a7d43d
Chinmay Bhosale <chinmayhbhosale02@gmail.com>
2024-11-11 23:36:31 +0530

    pushing the main files

---
+++ train_vit.py
@@ -0,0 +1,59 @@
+import torch
+import torchvision.transforms as tr
+from vit import MLP_Attention
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+from rich.progress import track
+import torch.nn as nn
+from rich import pretty
+########Transforms#######
+
+transforms = tr.Compose([tr.ToTensor(),tr.Lambda(lambda x:torch.cat([x,x,x],0))])
+
+########Hyperparameters and optimizer#########
+
+batch_size = 32
+num_heads = 8
+embed_dim = 64
+epochs = 100
+lr = 0.0001
+patch_size = 16
+img_size = 28
+model = MLP_Attention(n_embed=embed_dim,num_classes=10,patch_size=14)
+optimizer = torch.optim.Adam(model.parameters(),lr=lr)
+
+
+#######Dataset#######
+
+train_dataset = MNIST(root='/home/chinu_tensor/basic_vision_transformer/mnist',download=True,transform=transforms)
+test_dataset = MNIST(root='/home/chinu_tensor/basic_vision_transformer/mnist',download=True,train=False,transform=transforms)
+train_loader = DataLoader(train_dataset,batch_size=batch_size,shuffle=True) 
+test_loader = DataLoader(test_dataset,batch_size=batch_size,shuffle=True) 
+
+######## Training ########
+
+for epoch in track(range(epochs),description='Training'):
+        epoch_loss = 0 
+        for inps, target in train_loader:
+ 
+            logits = model(inps)
+            logits = logits.mean(dim=1)
+            batch_loss = nn.functional.cross_entropy(logits, target)
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+            
+            epoch_loss += batch_loss.item()
+
+        epoch_loss /= len(train_loader)
+        print(f"Loss:{epoch_loss}")
+
+######## Save Model dict #######
+
+torch.save(model.state_dict(),'./checkpoints/vit.pt')
+
+pretty.pprint(f'model saved in the checkpoints directory')
+
+
+
+
---
+++ vit.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+
+#Assuming data dim is [B,C,H,W] 
+
+
+
+class Patch_Gen(nn.Module):
+    
+
+    def __init__(self,embed_dim:int=64,patch_size:int=8):
+        super().__init__()
+        self.patch_mapper = nn.Conv2d(in_channels=3,out_channels=embed_dim,kernel_size=patch_size,stride=patch_size)
+        self.linear_1 = nn.Linear(embed_dim,embed_dim)
+    
+
+    def forward(self,x):
+        x = self.patch_mapper(x)
+        x = x.flatten(2) 
+        x = x.transpose(1,2)
+       
+        x = self.linear_1(x)
+        return x
+      
+
+
+class Main_Model(nn.Module):
+    def __init__(self,num_heads:int=8,embed_dim:int=64,patch_size:int=8):
+        super().__init__()
+        self.patch_embed = Patch_Gen(embed_dim=embed_dim,patch_size=patch_size)
+        self.sa_head = nn.MultiheadAttention(embed_dim=embed_dim,num_heads=num_heads,batch_first=True)
+    def forward(self,x):
+        x = self.patch_embed(x)
+        x,_ = self.sa_head(x,x,x) 
+        return x
+
+
+class MLP_Attention(nn.Module):
+    def __init__(self,n_embed:int,num_classes:int,patch_size:int=8):
+        super().__init__()
+        self.main = Main_Model(patch_size=patch_size,embed_dim=n_embed)
+        self.layering = nn.Sequential(
+            nn.Linear(n_embed,100),
+            nn.ReLU(),
+            nn.Linear(100,100),
+            nn.ReLU(),
+            nn.Linear(100,100),
+            nn.ReLU(),
+            nn.Linear(100,num_classes)
+            )
+    def forward(self,x):
+        logits = self.main(x)
+        logits = self.layering(logits)
+        return logits 
+
+
+
+
